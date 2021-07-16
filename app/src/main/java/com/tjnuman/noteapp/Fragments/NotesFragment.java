474125e672c68a3cc13297a654d68c839412ece5
TJ-numan <abdullahalnuman235@gmail.com>
2021-07-16 18:53:18 +0600

    going notesFragment to create note activity

--- app/src/main/java/com/tjnuman/noteapp/Fragments/NotesFragment.java
+++ app/src/main/java/com/tjnuman/noteapp/Fragments/NotesFragment.java
@@ -1,15 +1,22 @@
 package com.tjnuman.noteapp.Fragments;
 
+import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.tjnuman.noteapp.Activities.InsertNoteActivity;
 import com.tjnuman.noteapp.R;
 import com.tjnuman.noteapp.databinding.FragmentNotesBinding;
 
 
 public class NotesFragment extends Fragment {
+  FloatingActionButton newNotebutton;
 
 
     public NotesFragment() {
@@ -17,29 +24,23 @@ public class NotesFragment extends Fragment {
     }
 
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-
-    }
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_notes, container, false);
 
 
-        return inflater.inflate(R.layout.fragment_notes, container, false);
+        newNotebutton = view.findViewById(R.id.newnotebtn);
+
+        newNotebutton.setOnClickListener(v -> {
+
+            Intent intent = new Intent(getActivity(), InsertNoteActivity.class);
+            startActivity(intent);
+        });
 
 
 
-
-
-
-
-
-
+        return view;
     }
 }
